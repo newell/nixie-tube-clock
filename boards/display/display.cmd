d9a18e9ae6feecaa920873cb97c893f178b0f746
Newell Jensen <newell.jensen@gmail.com>
2017-11-27 22:09:06 -0800

    Add display pcb layout and gsch2pcb created files.

---
+++ boards/display/display.cmd
@@ -0,0 +1,191 @@
+# Pin name action command file
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element CONN13
+ChangePinName(CONN13, 5, 5)
+ChangePinName(CONN13, 3, 3)
+ChangePinName(CONN13, 1, 1)
+ChangePinName(CONN13, 4, 4)
+ChangePinName(CONN13, 2, 2)
+
+# Start of element N6
+ChangePinName(N6, 1, A)
+ChangePinName(N6, 2, 0)
+ChangePinName(N6, 3, 9)
+ChangePinName(N6, 4, 8)
+ChangePinName(N6, 5, 7)
+ChangePinName(N6, 6, 6)
+ChangePinName(N6, 7, 5)
+ChangePinName(N6, 8, 4)
+ChangePinName(N6, 9, 3)
+ChangePinName(N6, 10, 2)
+ChangePinName(N6, 11, 1)
+
+# Start of element CONN12
+ChangePinName(CONN12, 5, 5)
+ChangePinName(CONN12, 3, 3)
+ChangePinName(CONN12, 1, 1)
+ChangePinName(CONN12, 4, 4)
+ChangePinName(CONN12, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element CONN11
+ChangePinName(CONN11, 5, 5)
+ChangePinName(CONN11, 3, 3)
+ChangePinName(CONN11, 1, 1)
+ChangePinName(CONN11, 4, 4)
+ChangePinName(CONN11, 2, 2)
+
+# Start of element N5
+ChangePinName(N5, 1, A)
+ChangePinName(N5, 2, 0)
+ChangePinName(N5, 3, 9)
+ChangePinName(N5, 4, 8)
+ChangePinName(N5, 5, 7)
+ChangePinName(N5, 6, 6)
+ChangePinName(N5, 7, 5)
+ChangePinName(N5, 8, 4)
+ChangePinName(N5, 9, 3)
+ChangePinName(N5, 10, 2)
+ChangePinName(N5, 11, 1)
+
+# Start of element CONN10
+ChangePinName(CONN10, 5, 5)
+ChangePinName(CONN10, 3, 3)
+ChangePinName(CONN10, 1, 1)
+ChangePinName(CONN10, 4, 4)
+ChangePinName(CONN10, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element CONN9
+ChangePinName(CONN9, 5, 5)
+ChangePinName(CONN9, 3, 3)
+ChangePinName(CONN9, 1, 1)
+ChangePinName(CONN9, 4, 4)
+ChangePinName(CONN9, 2, 2)
+
+# Start of element N4
+ChangePinName(N4, 1, A)
+ChangePinName(N4, 2, 0)
+ChangePinName(N4, 3, 9)
+ChangePinName(N4, 4, 8)
+ChangePinName(N4, 5, 7)
+ChangePinName(N4, 6, 6)
+ChangePinName(N4, 7, 5)
+ChangePinName(N4, 8, 4)
+ChangePinName(N4, 9, 3)
+ChangePinName(N4, 10, 2)
+ChangePinName(N4, 11, 1)
+
+# Start of element CONN8
+ChangePinName(CONN8, 5, 5)
+ChangePinName(CONN8, 3, 3)
+ChangePinName(CONN8, 1, 1)
+ChangePinName(CONN8, 4, 4)
+ChangePinName(CONN8, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element CONN7
+ChangePinName(CONN7, 5, 5)
+ChangePinName(CONN7, 3, 3)
+ChangePinName(CONN7, 1, 1)
+ChangePinName(CONN7, 4, 4)
+ChangePinName(CONN7, 2, 2)
+
+# Start of element N3
+ChangePinName(N3, 1, A)
+ChangePinName(N3, 2, 0)
+ChangePinName(N3, 3, 9)
+ChangePinName(N3, 4, 8)
+ChangePinName(N3, 5, 7)
+ChangePinName(N3, 6, 6)
+ChangePinName(N3, 7, 5)
+ChangePinName(N3, 8, 4)
+ChangePinName(N3, 9, 3)
+ChangePinName(N3, 10, 2)
+ChangePinName(N3, 11, 1)
+
+# Start of element CONN6
+ChangePinName(CONN6, 5, 5)
+ChangePinName(CONN6, 3, 3)
+ChangePinName(CONN6, 1, 1)
+ChangePinName(CONN6, 4, 4)
+ChangePinName(CONN6, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element CONN5
+ChangePinName(CONN5, 5, 5)
+ChangePinName(CONN5, 3, 3)
+ChangePinName(CONN5, 1, 1)
+ChangePinName(CONN5, 4, 4)
+ChangePinName(CONN5, 2, 2)
+
+# Start of element N2
+ChangePinName(N2, 1, A)
+ChangePinName(N2, 2, 0)
+ChangePinName(N2, 3, 9)
+ChangePinName(N2, 4, 8)
+ChangePinName(N2, 5, 7)
+ChangePinName(N2, 6, 6)
+ChangePinName(N2, 7, 5)
+ChangePinName(N2, 8, 4)
+ChangePinName(N2, 9, 3)
+ChangePinName(N2, 10, 2)
+ChangePinName(N2, 11, 1)
+
+# Start of element CONN4
+ChangePinName(CONN4, 5, 5)
+ChangePinName(CONN4, 3, 3)
+ChangePinName(CONN4, 1, 1)
+ChangePinName(CONN4, 4, 4)
+ChangePinName(CONN4, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element CONN3
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 2, 2)
+
+# Start of element N1
+ChangePinName(N1, 1, A)
+ChangePinName(N1, 2, 0)
+ChangePinName(N1, 3, 9)
+ChangePinName(N1, 4, 8)
+ChangePinName(N1, 5, 7)
+ChangePinName(N1, 6, 6)
+ChangePinName(N1, 7, 5)
+ChangePinName(N1, 8, 4)
+ChangePinName(N1, 9, 3)
+ChangePinName(N1, 10, 2)
+ChangePinName(N1, 11, 1)
+
+# Start of element CONN2
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
